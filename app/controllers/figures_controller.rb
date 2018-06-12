add9aee236a7fb6f815071be50ccb6a8dec5e534
Joseph Parker <jparker010190@gmail.com>
2018-06-12 01:31:33 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,10 +3,10 @@ class FiguresController < ApplicationController
     @figures = Figure.all
     erb :'figures/index'
   end
-  get '/figures/new' do 
+  get '/figures/new' do
     erb :'figures/new'
   end
-  get '/figures/:id' do 
+  get '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     erb :'figures/show'
   end
@@ -19,6 +19,22 @@ class FiguresController < ApplicationController
       figure.landmarks.create(params[:landmark])
     end
 
+    redirect "/figures/#{figure.id}"
+  end
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = Figure.find_by_id(params[:id])
+    figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      figure.titles.create(params[:title])
+    end
+    unless params[:landmark][:name].empty?
+      figure.landmarks.create(params[:landmark])
+    end
     redirect "/figures/#{figure.id}"
   end
 end
