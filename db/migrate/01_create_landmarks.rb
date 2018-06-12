2e3637a8fc68183c22d1460a981a6313a316b00e
Joseph Parker <jparker010190@gmail.com>
2018-06-12 00:49:55 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -3,6 +3,8 @@ class CreateLandmarks < ActiveRecord::Migration
   def change
     create_table :landmark do |t|
       t.string :name
+      t.integer :figure_id
+      t.integer :year_completed
     end
   end
 end
